07a15f1b1b3c48cbbd16aa14bb1ca73c0a4b0d1a
ishicororo <ishicororo.tks@gmail.com>
2025-06-21 12:59:13 +0000

    game

--- game.js
+++ game.js
@@ -78,8 +78,8 @@ process.stdin.on('data',(input)=>{
         data.users[name].playingGame.othello=true;
         fs.writeFileSync('data.json',JSON.stringify(data,null,2));
         console.log("選択してください");
-        console.log("1：AIと対戦ー勝率");
-        console.log("2：AIと対戦ーコマの数");
+        console.log("1：AIと対戦ーコマの数");
+        console.log("2：AIと対戦ー勝率");
         console.log("3：2人で対戦");
         progress=4;
     }
@@ -92,24 +92,45 @@ process.stdin.on('data',(input)=>{
     }
     else{
         console.log(有効な数字を入力してください);
+        console.log("1:じゃんけん");
+        console.log("2:オセロ");
+        console.log("3:五目並べ");
     }
     }
     else if(progress===3){
         console.clear();
         if(input==="1"){
-            progress=6;
             console.log("AIはプレイするごとに学習して強くなります")
             console.log("じゃんけんを始めます、カタカナで入力してください");
             console.log("最初はグー、じゃんけん...");
+            progress=6;
         }
         else if(input==="2"){
-            progress=7;
             console.log("AIはプレイするほど強くなります");
             console.log("プレイする相手の名前を入力してください")
+            progress=7;
+        }
+        else{
+            console.log("有効な数字を入力してください");
+            console.log("1:AIと対戦する");
+            console.log("2:AIとともに相手と対戦する");
         }
     }
     else if(progress===4){
         console.clear();
+        if(input==="1"){
+            console.log("AIは常にAIのコマが最も大きくなる答えを出します");
+            console.log("これは初級者向けです");
+            progress=8;
+        }
+        else if(input==="2"){
+            console.log("AIは勝率が最も高くなる手を選びます")
+            console.log("これは上級者向けです");
+            progress=9;
+        }
+        else if(input==="3"){
+            console.log("他の人と対戦します")
+        }
     }
     else if(progress===5){
         console.clear();

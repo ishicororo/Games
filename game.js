3d3ddf67aa1e66208fe65d452212ef0582081e89
ishicororo <ishicororo.tks@gmail.com>
2025-06-21 11:07:02 +0000

    game

--- game.js
+++ game.js
@@ -4,9 +4,14 @@ let progress=1;
 process.stdin.setEncoding("utf8");
 console.log("こんにちは、ユーザー名を入力してください");
 process.stdin.on('data',(input)=>{
-    if(progress===1){
-    console.clear();
     input=input.trim();
+    console.clear();
+    if(input==="exit"){
+        console.log("ゲームを終了します。");
+        process.exit();
+    }
+    else{
+    if(progress===1){
     if(!(input in data.users)){
         data.users[input]={
             info:true,
@@ -18,7 +23,7 @@ process.stdin.on('data',(input)=>{
         console.log("1:じゃんけん");
         console.log("2:オセロ");
         console.log("3:五目並べ");
-        progress+=1;
+        progress=2;
     }
     else{
         console.log("こんにちは、"+input+"さん");
@@ -26,16 +31,17 @@ process.stdin.on('data',(input)=>{
         console.log("1:じゃんけん");
         console.log("2:オセロ");
         console.log("3:五目並べ");
-        progress+=1;
+        progress=2;
 
     }
 }
-    if(progress===2){
+    else if(progress===2){
         console.clear();
     if(input==="1"){
         console.log("どちらかを選択してください");
         console.log("1:AIと対戦する");
-        console.log("AIとともに相手と対戦する");
+        console.log("2:AIとともに相手と対戦する");
     }
     }
+}
 })

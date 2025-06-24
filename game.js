342e8fb5bc36dc42cd378235528430f700c75045
ishicororo <ishicororo.tks@gmail.com>
2025-06-24 12:46:18 +0000

    game

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
 const inquirer=require("inquirer");
 const fs=require("fs");
 const { type } = require("os");
+const { listenerCount } = require("process");
+const { default: Choices } = require("inquirer/lib/objects/choices");
 let data=JSON.parse(fs.readFileSync('data.json','utf8'));
 console.clear();
 function save(){fs.writeFileSync('data.json',JSON.stringify(data,null,2));}
@@ -20,24 +22,45 @@ if(data.display===true){
     ]).then(answers => {
         if(answers.answer===true){
             console.log("了解しました");
+            askName();
         }
         else{
             console.log("了解しました、次からは案内を表示しません");
             data.display=false;
             save();
+            askName();
         }
     })
 }
-inquirer.prompt([
+function askName(){inquirer.prompt([
     {
         type:'input',
         name:'name',
         message:"あなたの名前を入力してください",
     }
 ]).then(answers=>{
-    console.log(`こんにちは、${answers.name}さん`);
+    if((answers.name in data.users)){
+    console.log(`はじめまして、${answers.name}さん`);
     data.users[answers.name]={
         info:true,
     }
     save();
+}
+    else{
+        console.log(`こんにちは、${answers.name}さん`);
+    }
 })
+}
+function chooseGame(){
+    inquirer.prompt([
+        {
+        type:"list",
+        name: "game",
+        message:"ゲームを選択してください",
+        choices:["じゃんけん","オセロ","五目並べ"],
+        }
+    ]).then(answers=>{})
+}
+function jankennoOption(){}
+function othelloOption(){}
+function gomokuOption(){}
